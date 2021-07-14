5dbf00d9b2b75d3ea48820f1e3ea385f81d8d436
Purrbiscuit <spurr@articulate.com>
2021-07-14 19:15:17 -0400

    add course data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-const App = () =>
-  <div>
-    <h1>Hello World (Dockerized)</h1>
+import courses from './data/courses'
+
+const App = () => {
+  return <div>
+    {courses.map(course =>
+      <h1 key={course.number}>Category: {course.category},  Number: {course.number},  Description: {course.description},  Took: {course.took.toString()}</h1>
+    )}
   </div>
+}
 
 export default App
---
+++ src/data/courses.js
@@ -0,0 +1,11 @@
+export default [
+  { category: 'Major', number: 'CSC115', description: 'Python', took: true },
+  { category: 'Major', number: 'CSC141', description: 'Java I', took: true },
+  { category: 'Major', number: 'CSC142', description: 'Java II', took: false },
+  { category: 'Major', number: 'CSC240', description: 'Java III', took: false },
+  { category: 'Major', number: 'CSC241', description: 'Data Structures', took: false },
+  { category: 'Major', number: 'ESS200', description: 'Geology', took: true },
+  { category: 'GenEd', number: 'ART100', description: 'Music', took: false },
+  { category: 'GenEd', number: 'ENG369', description: 'English', took: true },
+  { category: 'GenEd', number: 'LIT200', description: 'History', took: false },
+]
