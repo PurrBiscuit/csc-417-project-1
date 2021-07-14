77b110762b225172c7ee7a06bad3da21aade3fdd
Purrbiscuit <spurr@articulate.com>
2021-07-14 19:25:07 -0400

    conditionally apply classes to course listing

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import courses from './data/courses'
 
 const App = () => {
   return <div>
-    {courses.map(course =>
-      <h1 key={course.number}>Category: {course.category},  Number: {course.number},  Description: {course.description},  Took: {course.took.toString()}</h1>
-    )}
+    {courses.map(course => {
+      const classes = course.took ? 'text-dark' : 'text-danger'
+
+      return <h1 key={course.number} className={classes}>Category: {course.category},  Number: {course.number},  Description: {course.description},  Took: {course.took.toString()}</h1>
+    })}
   </div>
 }
 
