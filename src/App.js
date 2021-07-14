cf0bf7cff489e5e03bd46ca3f2f35dbf706eb4f1
Purrbiscuit <spurr@articulate.com>
2021-07-14 19:44:27 -0400

    ugly, but functional

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useState } from 'react'
 import courses from './data/courses'
 
 const App = () => {
+  const [ showAllCourses, setShowAllCourses ] = useState(true)
+
+  const handleOnChange = () =>
+    setShowAllCourses(!showAllCourses)
+
   return <div>
-    {courses.map(course => {
+    <input type="checkbox" id="toggle" name="toggle-courses" onChange={handleOnChange}/>
+    <label for="toogle">Only show courses took already</label>
+
+    {courses.filter(course => showAllCourses || course.took)
+      .map(course => {
       const classes = course.took ? 'text-dark' : 'text-danger'
 
       return <h1 key={course.number} className={classes}>Category: {course.category},  Number: {course.number},  Description: {course.description},  Took: {course.took.toString()}</h1>
